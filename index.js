8f350f50f2b3a214092d754990aaaa0f6e11f51d
Nasara <dinnanitipa@gmail.com>
2026-09-21 16:24:59 +0200

    Increase JSON payload limit for website publishing

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const notifyUser = require("./services/notifyUser");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
 /* ================= LIMIT FILE SIZE (20MB) ================= */
 const upload = multer({
