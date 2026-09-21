c78643303635bebcf25a05342bee2b0246be523b
Nasara <dinnanitipa@gmail.com>
2026-09-21 17:44:09 +0200

    Use built-in fetch for Netlify publishing

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const hospitalRoutes = require("./routes/hospital");
 const restaurantRoutes = require("./routes/restaurant");
 
 const { createClient } = require("@supabase/supabase-js");
-const fetch = require("node-fetch");
 const AdmZip = require("adm-zip");
 const notifyUser = require("./services/notifyUser");
 
